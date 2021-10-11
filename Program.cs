980daf5ae4f4f128d772c2b7792b20d63130e45b
back.end.at@dzencode.com <back.end.at.dzencode@google.com>
2021-10-11 14:34:53 +0300

    firstCommit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Everyone needs a little LINQ in their life!");
+
+
+
+class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public int Age { get; set; }
+}
